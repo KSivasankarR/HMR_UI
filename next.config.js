e1fbda219fef00e0cdad11c17bf5cc34851e175b
SIVASANKAR REDDY KOTA <119029521+KSivasankarR@users.noreply.github.com>
2026-03-18 16:17:56 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-
+/*
 const ContentSecurityPolicy = `
-  default-src 'self' 'unsafe-inline' https://marriage.rs.ap.gov.in https://registration.ap.gov.in;
+  default-src 'self' 'unsafe-inline' https://marriage.rs.ap.gov.in http://registration.ap.gov.in https://registration.ap.gov.in http://10.10.120.190:4009 http://10.10.120.190:3000;
   script-src 'self' 'unsafe-inline';
-  child-src https://marriage.rs.ap.gov.in https://registration.ap.gov.in;
-  style-src 'self' 'unsafe-inline' https://marriage.rs.ap.gov.in https://registration.ap.gov.in https://fonts.googleapis.com https://www.gstatic.com;
+  child-src https://marriage.rs.ap.gov.in http://registration.ap.gov.in https://registration.ap.gov.in http://10.10.120.190:4009 http://10.10.120.190:3000;
+  style-src 'self' 'unsafe-inline' https://marriage.rs.ap.gov.in https://registration.ap.gov.in http://10.10.120.190:4009 http://10.10.120.190:3000 https://fonts.googleapis.com https://www.gstatic.com;
   font-src 'self' 'unsafe-inline' https://fonts.gstatic.com;
-  img-src * 'self' data: https:;
-`
+  img-src * 'self' data: http:;
+`*/
 
 const securityHeaders = [
       {key: 'X-Frame-Options',value: 'SAMEORIGIN'},
+	  /*
       {key: 'X-XSS-Protection',value: '1; mode=block'},
       {key: 'X-Content-Type-Options',value: 'nosniff'},
       {key: 'Content-Security-Policy',value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()},
       {key: 'Strict-Transport-Security',value: 'max-age=63072000; includeSubDomains; preload'} 
+	  */
 ]
 
 require("dotenv").config();
@@ -35,6 +37,7 @@ const nextConfig = {
   env:{
     API_URL:process.env.API_URL,
     PAYMENT_GATEWAY_URL:process.env.PAYMENT_GATEWAY_URL,
+    AADHAAR_API_URL:process.env.AADHAAR_API_URL,
     PORT:process.env.PORT,
     HMR_HOME_URL:process.env.HMR_HOME_URL,
     OWN_ESIGN_URL:process.env.OWN_ESIGN_URL,
